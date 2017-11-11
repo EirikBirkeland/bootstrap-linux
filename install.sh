8ad2235da2cc2349f551be552f1bc1869bc1386e
Eirik Birkeland <EirikBirkeland@users.noreply.github.com>
2017-11-11 13:27:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 echo "Update & upgrade:"
 sudo apt-get update && sudo apt-get upgrade
 echo "Installing commonly used packages:"
-sudo apt-get install htop vim ssh npm
+sudo apt-get install htop vim ssh npm nmap
