3025593040e3a858f88099fcb5054d78a9bca2f7
Eirik Birkeland <EirikBirkeland@users.noreply.github.com>
2017-11-16 18:10:01 +0100

    Update and rename configure-npm.sh to customize-npm-init.sh

--- customize-npm-init.sh
+++ customize-npm-init.sh
@@ -4,5 +4,5 @@ npm config set init.author.url "https://github.com/EirikBirkeland"
 
 npm config set init.license MIT
 
-// Reduce the chance of people adopting potentially immature NPM package
+# Reduce the chance of people adopting potentially immature NPM package
 npm config set init.version 0.0.1
