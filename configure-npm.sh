3b8fe4c3839ff4758f85a15e6b585f8249493e5a
Eirik Birkeland <EirikBirkeland@users.noreply.github.com>
2017-11-16 18:09:31 +0100

    Create configure-npm.sh

---
+++ configure-npm.sh
@@ -0,0 +1,8 @@
+npm config set init.author.name "Eirik Birkeland"
+
+npm config set init.author.url "https://github.com/EirikBirkeland"
+
+npm config set init.license MIT
+
+// Reduce the chance of people adopting potentially immature NPM package
+npm config set init.version 0.0.1
