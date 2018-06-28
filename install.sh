1d0fb7eb084235e31458b1f75b63746f758def16
Eirik Birkeland <birketrans@gmail.com>
2018-06-28 15:10:22 +0200

    update my theme

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 echo "Update & upgrade:"
 sudo apt-get update && sudo apt-get upgrade -y
-echo "Installing commonly used packages:"
+echo "Installing commonly used packages ..."
 sudo apt-get install htop vim ssh npm nmap git tmux curl zsh
 
-# Install oh-my-zsh
+echo "Installing oh-my-zsh ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Set up ~/.npm-global
+echo "Setting up ~/.npm-global ..."
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 
 source ~/.zshrc
+
+echo "Installing NVM for Nodejs"
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
